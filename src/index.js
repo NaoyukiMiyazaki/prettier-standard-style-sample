fd2b51f0d5075cc312f40400fd63729882c20df7
Naoyuki Miyazaki <miyazaki@buddying.jp>
2018-03-04 15:46:24 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-function log(a, b) {
-  console.log(a, b)
-}
-var i = 'hi'
-if (i === 'hi') {
-  log('hi', 'hihi')
-}
+window.addEventListener('load', () => {
+  function log(a, b) {
+    console.log(a, b)
+  }
+  var i = 'hi'
+  if (i === 'hi') {
+    log('hi', 'hihi')
+  }
+})
